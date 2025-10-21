4cd8f3b26010a69970d1f363f5203ad4226965f6
SyedNaveedulhasan <syednaveedulhasan123@gmail.com>
2025-10-21 23:56:46 +0500

    project completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,30 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBLjCkD09Nwm0rC1eKB_yWIqOPiwyjZJZU",
+  authDomain: "personal-finance-tracker-29552.firebaseapp.com",
+  projectId: "personal-finance-tracker-29552",
+  storageBucket: "personal-finance-tracker-29552.firebasestorage.app",
+  messagingSenderId: "911957035745",
+  appId: "1:911957035745:web:c9c7ce71d3dcaa28b945a0",
+  measurementId: "G-8S9F1QXHJ0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-let analytics;
-isSupported().then((yes) => {
-  if (yes) analytics = getAnalytics(app);
-});
-
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-
-export { db, auth, provider, doc, setDoc };
+export {db, auth, provider, doc, setDoc};
